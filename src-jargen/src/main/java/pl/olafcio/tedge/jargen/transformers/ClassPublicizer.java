ef4bf514c07f9bcda348875aa9bcd268ef89f21c
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-07-26 05:31:16 +0200

    [ClassPublicizer] Enum to class conversion

--- src-jargen/src/main/java/pl/olafcio/tedge/jargen/transformers/ClassPublicizer.java
+++ src-jargen/src/main/java/pl/olafcio/tedge/jargen/transformers/ClassPublicizer.java
@@ -11,7 +11,7 @@ public class ClassPublicizer extends ClassVisitor {
 
     @Override
     public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-        int ch = 2;
+        int ch = 3;
 
         if ((access & Opcodes.ACC_PROTECTED) == Opcodes.ACC_PROTECTED)
             access = (access - Opcodes.ACC_PROTECTED) | Opcodes.ACC_PUBLIC;
@@ -26,14 +26,21 @@ public class ClassPublicizer extends ClassVisitor {
             access -= Opcodes.ACC_RECORD;
         else ch--;
 
+        var _enum = (access & Opcodes.ACC_ENUM) == Opcodes.ACC_ENUM;
+        if (_enum)
+            access -= Opcodes.ACC_ENUM;
+        else ch--;
+
+        String objectType = record                                                      ? "-record   " :
+                            _enum                                                       ? "-enum     " :
+                            (access & Opcodes.ACC_INTERFACE)  == Opcodes.ACC_INTERFACE  ? "interface " :
+                            (access & Opcodes.ACC_ANNOTATION) == Opcodes.ACC_ANNOTATION ? "@interface" :
+                                                                                           "class    ";
+
         if (ch > 0)
             if (Main.verbose)
                 IO.println("[+] public  %s %s".formatted(
-                        record                                                      ? "-record   " :
-                        (access & Opcodes.ACC_INTERFACE)  == Opcodes.ACC_INTERFACE  ? "interface " :
-                        (access & Opcodes.ACC_ENUM)       == Opcodes.ACC_ENUM       ? "enum      " :
-                        (access & Opcodes.ACC_ANNOTATION) == Opcodes.ACC_ANNOTATION ? "@interface" :
-                                                                                      "class     ",
+                        objectType,
                         name
                 ));
 
@@ -41,14 +48,7 @@ public class ClassPublicizer extends ClassVisitor {
             access -= Opcodes.ACC_FINAL;
 
             if (Main.verbose)
-                IO.println("[+] mutable %s %s".formatted(
-                        record                                                      ? "-record   " :
-                        (access & Opcodes.ACC_INTERFACE)  == Opcodes.ACC_INTERFACE  ? "interface " :
-                        (access & Opcodes.ACC_ENUM)       == Opcodes.ACC_ENUM       ? "enum      " :
-                        (access & Opcodes.ACC_ANNOTATION) == Opcodes.ACC_ANNOTATION ? "@interface" :
-                                                                                      "class     ",
-                        name
-                ));
+                IO.println("[+] mutable %s %s".formatted(objectType, name));
         }
 
         super.visit(version, access, name, signature, superName, interfaces);
