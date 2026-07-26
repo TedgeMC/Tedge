393607704d289e983db6110d9092dd46aca5ee5e
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-07-26 02:24:36 +0200

    [ClassPublicizer] Unfinalization

--- src-jargen/src/main/java/pl/olafcio/tedge/jargen/transformers/ClassPublicizer.java
+++ src-jargen/src/main/java/pl/olafcio/tedge/jargen/transformers/ClassPublicizer.java
@@ -35,6 +35,19 @@ public class ClassPublicizer extends ClassVisitor {
                     name
             ));
 
+        if ((access & Opcodes.ACC_FINAL) == Opcodes.ACC_FINAL) {
+            access -= Opcodes.ACC_FINAL;
+
+            IO.println("[+] mutable %s %s".formatted(
+                    record                                                      ? "-record   " :
+                    (access & Opcodes.ACC_INTERFACE)  == Opcodes.ACC_INTERFACE  ? "interface " :
+                    (access & Opcodes.ACC_ENUM)       == Opcodes.ACC_ENUM       ? "enum      " :
+                    (access & Opcodes.ACC_ANNOTATION) == Opcodes.ACC_ANNOTATION ? "@interface" :
+                                                                                  "class     ",
+                    name
+            ));
+        }
+
         super.visit(version, access, name, signature, superName, interfaces);
     }
 
