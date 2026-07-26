6b2ab0839faab994707ebe98f8fd9487fec8a68b
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-07-26 02:22:01 +0200

    [ClassPublicizer] Record to class conversion

--- src-jargen/src/main/java/pl/olafcio/tedge/jargen/transformers/ClassPublicizer.java
+++ src-jargen/src/main/java/pl/olafcio/tedge/jargen/transformers/ClassPublicizer.java
@@ -10,25 +10,30 @@ public class ClassPublicizer extends ClassVisitor {
 
     @Override
     public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-        print:
-        {
-            if ((access & Opcodes.ACC_PROTECTED) == Opcodes.ACC_PROTECTED)
-                access |= Opcodes.ACC_PROTECTED;
-            else if ((access & Opcodes.ACC_PRIVATE) == Opcodes.ACC_PRIVATE)
-                access |= Opcodes.ACC_PRIVATE;
-            else if ((access & Opcodes.ACC_PUBLIC) != Opcodes.ACC_PUBLIC)
-                access |= Opcodes.ACC_PUBLIC;
-            else break print;
+        int ch = 2;
 
+        if ((access & Opcodes.ACC_PROTECTED) == Opcodes.ACC_PROTECTED)
+            access |= Opcodes.ACC_PROTECTED;
+        else if ((access & Opcodes.ACC_PRIVATE) == Opcodes.ACC_PRIVATE)
+            access |= Opcodes.ACC_PRIVATE;
+        else if ((access & Opcodes.ACC_PUBLIC) != Opcodes.ACC_PUBLIC)
+            access |= Opcodes.ACC_PUBLIC;
+        else ch--;
+
+        var record = (access & Opcodes.ACC_RECORD) == Opcodes.ACC_RECORD;
+        if (record)
+            access -= Opcodes.ACC_RECORD;
+        else ch--;
+
+        if (ch > 0)
             IO.println("[+] public  %s %s".formatted(
+                    record                                                      ? "-record   " :
                     (access & Opcodes.ACC_INTERFACE)  == Opcodes.ACC_INTERFACE  ? "interface " :
                     (access & Opcodes.ACC_ENUM)       == Opcodes.ACC_ENUM       ? "enum      " :
-                    (access & Opcodes.ACC_RECORD)     == Opcodes.ACC_RECORD     ? "record    " :
                     (access & Opcodes.ACC_ANNOTATION) == Opcodes.ACC_ANNOTATION ? "@interface" :
                                                                                   "class     ",
                     name
             ));
-        }
 
         super.visit(version, access, name, signature, superName, interfaces);
     }
